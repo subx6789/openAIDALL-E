1dcd2886f88c06c4697754f981e3bb81fd16cfc8
Subhajit Sarkar <117716545+subx6789@users.noreply.github.com>
2023-01-22 09:20:31 +0530

    Update Home.jsx

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -20,7 +20,7 @@ const Home = () => {
     const fetchPosts = async () => {
       setLoading(true);
       try {
-        const response = await fetch("http://localhost:3000/api/v1/post", {
+        const response = await fetch("https://dall-e-2lqd.onrender.com/api/v1/post", {
           method: "GET",
           headers: {
             "Content-Type": "application/json",
